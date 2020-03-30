123a006f1475caaad167b30198f4eaaea14ff6de
Sander Demihhovski <demihhovski@mail.ru>
2020-03-30 15:03:36 +0300

    Ask User Name

--- TARpv19_Demihhovski/Program.cs
+++ TARpv19_Demihhovski/Program.cs
@@ -6,7 +6,11 @@ namespace TARpv19_Demihhovski
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Please, enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + "!");
+
 
             Console.ReadLine();
         }
