61c78e7d75fcd46b4aa6764ab61e96dd772e19e7
Sander Demihhovski <demihhovski@mail.ru>
2020-03-30 14:53:55 +0300

    Create Hello App

---
+++ TARpv19_Demihhovski/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TARpv19_Demihhovski
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            Console.ReadLine();
+        }
+    }
+}
